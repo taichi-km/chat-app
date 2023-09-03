dfe1a0270193be79f2c0448562cea59f2ea08fed
taichi-km <torinos947@gmail.com>
2023-09-03 17:25:57 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,18 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path  #保存に成功したらルートバスにリダイレクト
+    else
+      rendeer :new, status: :unprocessable_entity  #失敗したらrooms/new.heml.erbを表示
+    end
+  end
+
+  private
+
+  def room_params  #保存する前に許可する前に許可するパラメーターを指定する
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+
+  validates :names, presence: :true  #validatesメソッドを用いて、nameが空の場合はdbに保存しないというバリデーションを設定
+  
 end
