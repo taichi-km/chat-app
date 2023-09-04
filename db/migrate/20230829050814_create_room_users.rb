0a3be45c54db51c1e98eacab2782ebd13c2de2f4
taichi-km <torinos947@gmail.com>
2023-09-04 15:44:21 +0900

    messagesテーブルの作成

--- db/migrate/20230829050814_create_room_users.rb
+++ db/migrate/20230829050814_create_room_users.rb
@@ -2,7 +2,7 @@ class CreateRoomUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :room_users do |t|
       t.references :user, null: false, foreign_key: true   #references型は外部キーのカラムを追加する際に用いる
-      t.references :room, null: false, foreign_key: true   #foreign: trueを記述して外部キーの制約を設定する
+      t.references :room, null: false, foreign_key: true   #foreign_key:trueを記述して外部キーがないとdbに保存できないようにする
       t.timestamps
     end
   end
