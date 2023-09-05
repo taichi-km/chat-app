ffd19cd78eae250df9644bfa31b9a777bfcc1952
taichi-km <torinos947@gmail.com>
2023-09-05 14:02:49 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,7 @@
 class Room < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
-  has_many :messages
+  has_many :room_users,  dependent: :destroy  #roomが削除されたとき、中間テーブルも削除される
+  has_many :users,      through: :room_users
+  has_many :messages,  dependent: :destroy  #roomが削除されたとき、messagesテーブルも削除される
 
-  validates :names, presence: :true  #validatesメソッドを用いて、nameが空の場合はdbに保存しないというバリデーションを設定
-
+  validates :name, presence: true  #validatesメソッドを用いて、nameが空の場合はdbに保存しないというバリデーションを設定
 end
