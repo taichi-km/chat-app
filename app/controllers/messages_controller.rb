5e95c0758e36d4af59b7b46448c106513e683d91
taichi-km <torinos947@gmail.com>
2023-09-05 10:42:52 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
 class MessagesController < ApplicationController
   def index
+    @room = Room.find(params[:room_id])
+    @message = Message.new
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    #@room.messages.newでチャットルームに紐づいたメッセージのインスタンスを生成し、messages_paramsを引用してprivateメソッドを呼び出す
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def message_params  #messageの内容(content)をmessagesテーブルへ保存できるようにする
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    #パラメーターの中にログインしているユーザーのidと紐づいたcontentを受け取れるように許可する
+  end
+
+
+    
+
 end
